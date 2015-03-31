ceef23002eaa2e8fe398806ac5241d7a0c66b7ec
Max Gordon <gordonmaxc@gmail.com>
2015-03-31 11:53:34 -0700

    get user activities

--- app/controllers/fitbit_auth_controller.rb
+++ app/controllers/fitbit_auth_controller.rb
@@ -7,8 +7,35 @@ class FitbitAuthController < ApplicationController
     # Callback from Fitbit Oauth
     oauth_token = params[:oauth_token]
     oauth_verifier = params[:oauth_verifier]
+
+    # User Information and Access Credentials
     fitbit_data  = JSON.parse(MultiJson.encode(request.env['omniauth.auth']))
 
-    render json:fitbit_data
+    # Get User Activity Information
+    activities = get_user_activities(fitbit_data)
+
+    render json:activities
+  end
+
+private
+  def get_user_activities(fitbit_data)
+    fitbit_user_id = fitbit_data["uid"]
+    user_secret = fitbit_data["credentials"]["secret"]
+    user_token = fitbit_data["credentials"]["token"]
+
+    # Store this information in you user model for
+    # logins in the future.
+    client = Fitgem::Client.new({
+      consumer_key: ENV['FITBIT_CLIENT_KEY'],
+      consumer_secret: ENV['FITBIT_CLIENT_SECRET'],
+      token: user_token,
+      secret: user_secret,
+      user_id: fitbit_user_id,
+    })
+
+    access_token = client.reconnect(user_token, user_secret)
+
+    # client.activities_on_date('2015-03-25') <- Specific Date
+    client.activities_on_date('today')
   end
 end
